e52d215261d60aa2fdd14f4272f1428f72f19f68
TANKING <king@likeyunba.com>
2018-12-19 10:17:36 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+//api接口
+$url = "server.php";
+//发送请求
+function httpGet($url){
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_TIMEOUT, 500);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, true);
+    curl_setopt($curl, CURLOPT_URL, $url);
+    $res = curl_exec($curl);
+    curl_close($curl);
+    return $res;
+}
+//发送
+$str = httpGet($url);
+//返回接口的数据
+echo $str;
+?>
